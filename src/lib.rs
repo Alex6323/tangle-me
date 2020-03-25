0d6f8159e694333c60eca94411cf7c3fea823c12
/alex/ <Al3k5@protonmail.com>
2020-03-25 11:36:08 +0100

    Updated comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Properties of this crate:
 //! * uses HashMap's
-//! * u64 node identifiers
+//! * derives an u64 id from the transaction hash to prevent re-hashing of hashmap keys
 
 #![allow(dead_code)]
 
@@ -35,9 +35,8 @@ impl TransactionHash {
         });
         id
     }
-    // TODO: implement this function to achieve better collision resistence
     // NOTE: 5 trits/byte * 8 = 40 trits or ~13 trytes per u64, which should
-    // be collision resitent enough for an in-memory Tangle.
+    // be collision resistent enough for an in-memory Tangle.
     pub fn get_t5b1_id(&self) -> u64 {
         let t5b1: T5B1 = self.0.clone().into();
         assert!(t5b1.len() >= 8);
@@ -52,7 +51,6 @@ impl TransactionHash {
     }
 }
 
-// convert the first 8 trytes to `TransactionHash` to T5B1 encoding
 impl fmt::Debug for TransactionHash {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt(f)
