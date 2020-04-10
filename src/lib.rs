d28ce5fe8a051c413d7ae982ad974cacc0d5c8d3
/alex/ <Al3k5@protonmail.com>
2020-04-11 00:28:04 +0200

    Additional checks in the bundle walker

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,14 @@ type Hash = u64;
 pub struct BundledTransaction {
     hash: Hash,        // transaction hash
     index: usize,      // position in the bundle
-    last_index: usize, // size of the bundle
+    last: usize,       // size of the bundle
     bundle_hash: Hash, // Kerl bundle hash
     solid: bool,       // past cone fully available and referencing solid entry points
 }
 
 impl BundledTransaction {
     pub fn size(&self) -> usize {
-        self.last_index + 1
+        self.last + 1
     }
 }
 
@@ -33,22 +33,43 @@ impl std::hash::Hash for BundledTransaction {
 }
 
 impl<'a> Tangle<'a, BundledTransaction> {
-    //
+    // TODO: this function should return an iterator `BundleWalk`.
     pub fn walk_bundle(&self, bundle_tail: &'a BundledTransaction) -> Option<&'a BundledTransaction> {
         // NOTE: ensure that this function only gets called for tails
         assert_eq!(0, bundle_tail.index);
         let bundle_size = bundle_tail.size();
+        let bundle_hash = bundle_tail.bundle_hash;
+        let bundle_last = bundle_tail.last;
 
-        // tail == head
+        // tail == head?
         if bundle_size == 1 {
             return Some(bundle_tail);
         }
 
         let mut node = bundle_tail;
-        for _ in 0..bundle_tail.last_index {
+        let mut index = node.index;
+
+        for _ in 0..bundle_last {
             node = match self.get_trunk(node) {
                 Ok(has_trunk) => match has_trunk {
-                    Some(trunk) => trunk,
+                    Some(trunk) => {
+                        // NOTE: make sure all transactions in the bundle have the same bundle hash
+                        if trunk.bundle_hash != bundle_hash {
+                            return None;
+                        }
+                        // NOTE: make sure `last_index` is the same
+                        if trunk.last != bundle_last {
+                            return None;
+                        }
+
+                        // NOTE: make sure its `index` is increasing
+                        if trunk.index != index + 1 {
+                            return None;
+                        }
+
+                        index = trunk.index;
+                        trunk
+                    }
                     None => return None,
                 },
                 Err(()) => return None,
@@ -69,35 +90,35 @@ mod tests {
         let parent_bundle_tail_a = BundledTransaction {
             hash: 2000,
             index: 0,
-            last_index: 0,
+            last: 0,
             bundle_hash: 7777,
             solid: false,
         };
         let parent_bundle_tail_b = BundledTransaction {
             hash: 2001,
             index: 0,
-            last_index: 0,
+            last: 0,
             bundle_hash: 8888,
             solid: false,
         };
         let bundle_head = BundledTransaction {
             hash: 1002,
             index: 2,
-            last_index: 2,
+            last: 2,
             bundle_hash: 9999,
             solid: false,
         };
         let bundle_body = BundledTransaction {
             hash: 1001,
             index: 1,
-            last_index: 2,
+            last: 2,
             bundle_hash: 9999,
             solid: false,
         };
         let bundle_tail = BundledTransaction {
             hash: 1000,
             index: 0,
-            last_index: 2,
+            last: 2,
             bundle_hash: 9999,
             solid: false,
         };
